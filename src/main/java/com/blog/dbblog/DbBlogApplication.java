3822f596fba2f9d71ff342f7d2f040f7813c68de
xiwuqi <qiyuewuxi@yeah.net>
2023-08-16 01:26:28 +0800

    后端项目搭建

---
+++ src/main/java/com/blog/dbblog/DbBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.dbblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/dbblog/DbBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.dbblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
