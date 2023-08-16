86201509a4a782653ef9f10b38e51263166ad3cd
xiwuqi <qiyuewuxi@yeah.net>
2023-08-16 22:35:39 +0800

    增加了密码非空和手机号规范验证

--- src/main/java/com/blog/dbblog/controller/UserController.java
+++ src/main/java/com/blog/dbblog/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.blog.dbblog.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.blog.dbblog.entity.User;
 import com.blog.dbblog.service.UserService;
 import com.blog.dbblog.util.JsonResult;
+import com.blog.dbblog.util.MD5Util;
+import com.blog.dbblog.util.PhoneUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +40,15 @@ public class UserController {
      */
     @PostMapping("/create")
     public JsonResult<Object> userCreate(@RequestBody @Valid User user) {
+        if (StrUtil.isEmpty(user.getPassWord())) {
+            return JsonResult.error("密码为空，请填写密码！");
+        }
+        //密码加密存储
+        user.setPassWord(MD5Util.MD5(user.getPassWord()));
+        //判断手机号，这里用hutool工具类也可以
+        if (!PhoneUtil.checkMobile(user.getPhone())) {
+            return JsonResult.error("手机号码格式错误！");
+        }
         userService.createUser(user);
         return JsonResult.success();
     }
@@ -48,6 +60,15 @@ public class UserController {
      */
     @PostMapping("/update")
     public JsonResult<Object> userUpdate(@RequestBody @Valid User user) {
+        if (StrUtil.isEmpty(user.getPassWord())) {
+            return JsonResult.error("密码为空，请填写密码！");
+        }
+        //密码加密存储
+        user.setPassWord(MD5Util.MD5(user.getPassWord()));
+        //判断手机号，这里用hutool工具类也可以
+        if (!PhoneUtil.checkMobile(user.getPhone())) {
+            return JsonResult.error("手机号码格式错误！");
+        }
         userService.updateUser(user);
         return JsonResult.success();
     }
@@ -63,4 +84,5 @@ public class UserController {
     }
 
 }
+
 
