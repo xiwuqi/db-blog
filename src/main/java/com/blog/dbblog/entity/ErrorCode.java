3016abeeb5275fb40adfaa6616f4581347e7d32a
xiwuqi <qiyuewuxi@yeah.net>
2023-08-20 21:36:02 +0800

    创建错误码类，用来统一规范返回的错误信息

---
+++ src/main/java/com/blog/dbblog/entity/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.blog.dbblog.entity;
+
+/**
+ * 错误码
+ *
+ * @author wuxi
+ * @create 2023-08-20
+ */
+public enum ErrorCode {
+    SUCCESS("成功", 200),
+    NOT_LOGIN("未登录", 100),
+    ERROR_CODE("未定义错误", 101),
+    USER_NOT_EXIST("用户不存在", 102);
+
+    private int code;
+    private String msg;
+    private ErrorCode(String msg,int code){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
