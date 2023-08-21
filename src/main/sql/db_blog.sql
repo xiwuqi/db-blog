1718273d2a94b826295ac0de4c56c1eb7484afb4
xiwuqi <qiyuewuxi@yeah.net>
2023-08-21 22:07:22 +0800

    调整缩进

--- src/main/sql/db_blog.sql
+++ src/main/sql/db_blog.sql
@@ -131,17 +131,17 @@ CREATE TABLE `person_operation_log` (
 
 DROP TABLE IF EXISTS `person_login_log`;
 CREATE TABLE `person_login_log` (
-    `id`                BIGINT(20)         NOT NULL 	PRIMARY KEY AUTO_INCREMENT    		COMMENT '登录访问id',
-    `login_name`     	  VARCHAR(50)    		 NULL		DEFAULT ''                		COMMENT '登录账号',
+    `id`                BIGINT(20)           NOT NULL 	PRIMARY KEY AUTO_INCREMENT    	COMMENT '登录访问id',
+    `login_name`     	VARCHAR(50)    		 NULL		DEFAULT ''                		COMMENT '登录账号',
     `ip_address`        VARCHAR(128)   		 NULL		DEFAULT ''                		COMMENT '登录IP地址',
     `login_location`    VARCHAR(255)  		 NULL       DEFAULT ''                		COMMENT '登录地点',
     `browser_type`      VARCHAR(50)   		 NULL       DEFAULT ''                		COMMENT '浏览器类型',
-    `os`             	  VARCHAR(50)   		 NULL 	    DEFAULT ''                		COMMENT '操作系统',
+    `os`             	VARCHAR(50)   		 NULL 	    DEFAULT ''                		COMMENT '操作系统',
     `login_status`      TINYINT       		 NULL 	    DEFAULT 0               		COMMENT '登录状态，默认0, 0-成功, 1-失败',
-    `create_time`       DATETIME            	 NULL 	    DEFAULT CURRENT_TIMESTAMP       COMMENT '创建时间'
+    `create_time`       DATETIME             NULL 	    DEFAULT CURRENT_TIMESTAMP       COMMENT '创建时间'
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
   COLLATE = utf8mb4_bin
   ROW_FORMAT = Dynamic
-  COMMENT '登录日志表';
+    COMMENT '登录日志表';
 
