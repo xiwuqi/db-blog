b707823f77cc543136d377322de62b008e370428
xiwuqi <qiyuewuxi@yeah.net>
2023-08-24 02:14:58 +0800

    新增文件上传方法，新增一种文章创建的方法

--- src/main/java/com/blog/dbblog/service/Impl/ArticleServiceImpl.java
+++ src/main/java/com/blog/dbblog/service/Impl/ArticleServiceImpl.java
@@ -1,19 +1,28 @@
 package com.blog.dbblog.service.Impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.blog.dbblog.bo.ArticleBO;
+import com.blog.dbblog.bo.ArticleInsertBO;
 import com.blog.dbblog.config.mail.MailInfo;
 import com.blog.dbblog.config.mail.SendMailConfig;
 import com.blog.dbblog.entity.*;
 import com.blog.dbblog.mapper.ArticleMapper;
 import com.blog.dbblog.service.*;
+import com.blog.dbblog.util.FileUtils;
 import com.github.pagehelper.PageHelper;
 import lombok.extern.log4j.Log4j2;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.io.*;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -45,6 +54,20 @@ public class ArticleServiceImpl implements ArticleService {
     TagService tagService;
     @Resource
     CategoryService categoryService;
+
+    /**
+     * 本地路径
+     */
+    @Value("${upload.local.path}")
+    private String localPath;
+
+    /**
+     * 访问url
+     */
+    @Value("${upload.local.url}")
+    private String localUrl;
+
+    private static final String ARTICLE = "articles/";
 
     /**
      * key:文章id
@@ -101,8 +124,8 @@ public class ArticleServiceImpl implements ArticleService {
     *  在xml中就被完成赋值了 */
     @Override
     public void saveArticle(Article article) {
-        System.out.println("\nTagIdList的值："+ article.getTagIdList().toString());
-        System.out.println("CategoryIdList的值:"+article.getCategoryIdList().toString()+"\n");
+//        System.out.println("\nTagIdList的值："+ article.getTagIdList().toString());
+//        System.out.println("CategoryIdList的值:"+article.getCategoryIdList().toString()+"\n");
         // 创建文章记录
         articleMapper.createArticle(article);
         articleMap.put(article.getId(), article);
@@ -198,4 +221,179 @@ public class ArticleServiceImpl implements ArticleService {
         }
         return article;
     }
+
+    @Override
+    public String uploadFile(MultipartFile file) {
+        try {
+            // 获取文件md5值
+            String md5 = FileUtils.getMd5(file.getInputStream());
+            // 获取文件扩展名
+            String extName = FileUtils.getExtName(file.getOriginalFilename());
+            // 重新生成文件名
+            String fileName = md5 + extName;
+            // 判断文件是否已存在
+            if (!exists(ARTICLE + fileName)) {
+                // 不存在则继续上传
+                upload(ARTICLE, fileName, file.getInputStream());
+            }
+
+            // 返回文件访问路径
+            return getFileAccessUrl(ARTICLE + fileName);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("文件上传失败");
+        }
+        return null;
+    }
+
+    @Override
+    public void insertOrUpdateArticle(ArticleInsertBO bo) throws Exception {
+        //分类添加
+        Article article = BeanUtil.copyProperties(bo, Article.class);
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        User user = userService.getUserByUserName(username);
+        article.setUserId(user.getId());
+        article.setAuthor(user.getUserName());
+        article.setViews(0L);
+        article.setTotalWords(0L);
+        if (bo.getId() != null) {
+            articleMapper.updateArticle(article);
+        } else {
+            articleMapper.createArticle(article);
+        }
+        articleMap.put(article.getId(), article);
+        // 增加文章分类
+        saveCategories(bo, article.getId());
+
+        // 添加文章标签
+        saveTags(bo, article.getId());
+
+        //添加文章发送邮箱提醒
+        try {
+            String content = "【{0}】您好：\n" +
+                    "您已成功发布了标题为: {1} 的文章 \n" +
+                    "请注意查收！\n";
+            MailInfo build = MailInfo.builder()
+                    .receiveMail(user.getEmail())
+                    .content(MessageFormat.format(content, user.getUserName(), article.getTitle()))
+                    .title("文章发布")
+                    .build();
+            SendMailConfig.sendMail(build);
+        } catch (Exception e) {
+            log.error("邮件发送失败{}", e.getMessage());
+        }
+
+    }
+
+    private void saveCategories(ArticleInsertBO bo, Integer articleId) throws Exception {
+        //首先判断是不是更新文章
+        if (bo.getId() == null) {
+            articleCategoryService.deleteCategory(bo.getId());
+        }
+        //添加文章标签
+        List<String> categoryNameList = bo.getCategoryNameList();
+        List<Integer> categoryIdList = new ArrayList<>();
+
+        if (CollUtil.isNotEmpty(categoryNameList)) {
+            //先查看添加的标签数据库里有没有
+            for (String categoryName : categoryNameList) {
+                Category one = categoryService.getCategoryByName(categoryName);
+                if (one == null) {
+                    Category category = new Category();
+                    category.setCategoryName(categoryName);
+                    categoryService.saveCategory(category);
+                    categoryIdList.add(category.getCategoryId());
+                } else {
+                    categoryIdList.add(one.getCategoryId());
+                }
+            }
+        }
+        articleCategoryService.deleteCategory(articleId);
+        if (categoryIdList != null) {
+            List<ArticleCategory> articleCategoryList = categoryIdList.stream().map(categoryId -> ArticleCategory.builder()
+                    .categoryId(categoryId)
+                    .articleId(articleId)
+                    .build()).collect(Collectors.toList());
+            articleCategoryService.insertBatch(articleCategoryList);
+        }
+    }
+
+    private void saveTags(ArticleInsertBO bo, Integer articleId) throws Exception {
+        //首先判断是不是更新文章
+        if (bo.getId() == null) {
+            articleTagService.deleteTag(bo.getId());
+        }
+        //添加文章标签
+        List<String> tagNameList = bo.getTagNameList();
+        List<Integer> tagIdList = new ArrayList<>();
+
+        if (CollUtil.isNotEmpty(tagNameList)) {
+            //先查看添加的标签数据库里有没有
+            for (String tagName : tagNameList) {
+                Tag one = tagService.findByTagName(tagName);
+                if (one == null) {
+                    Tag tag = new Tag();
+                    tag.setTagName(tagName);
+                    tagService.saveTag(tag);
+                    tagIdList.add(tag.getId());
+                } else {
+                    tagIdList.add(one.getId());
+                }
+            }
+        }
+        articleTagService.deleteTag(articleId);
+        if (tagIdList != null) {
+            List<ArticleTag> articleTagList = tagIdList.stream().map(tagId -> ArticleTag.builder()
+                    .tagId(tagId)
+                    .articleId(articleId)
+                    .build()).collect(Collectors.toList());
+            articleTagService.insertBatch(articleTagList);
+        }
+    }
+
+    private void upload(String path, String fileName, InputStream inputStream) throws IOException {
+        File directory = new File(localPath + path);
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                log.error("创建目录失败");
+            }
+        }
+        // 写入文件
+        File file = new File(localPath + path + fileName);
+        if (file.createNewFile()) {
+            BufferedInputStream bis = new BufferedInputStream(inputStream);
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = bis.read(bytes)) != -1) {
+                bos.write(bytes, 0, length);
+            }
+            bos.flush();
+            inputStream.close();
+            bis.close();
+            bos.close();
+        }
+    }
+
+    /**
+     * 判断文件是否存在
+     *
+     * @param filePath 文件路径
+     * @return
+     */
+    public Boolean exists(String filePath){
+        return new File(localPath + filePath).exists();
+    }
+
+    /**
+     * 获取文件访问url
+     *
+     * @param filePath 文件路径
+     * @return
+     */
+    public String getFileAccessUrl(String filePath) {
+        return localUrl + localPath + filePath;
+    }
+
 }
