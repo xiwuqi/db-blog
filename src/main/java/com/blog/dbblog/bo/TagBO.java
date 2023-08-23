b724b6f74bbc6c54854f3633c330a4cd02a93606
xiwuqi <qiyuewuxi@yeah.net>
2023-08-24 02:02:37 +0800

    新增标签的查询业务类

---
+++ src/main/java/com/blog/dbblog/bo/TagBO.java
@@ -0,0 +1,18 @@
+package com.blog.dbblog.bo;
+
+import lombok.Data;
+
+/**
+ * 标签查询条件
+ *
+ * @author wuxi
+ * @create 2023-08-23
+ **/
+@Data
+public class TagBO {
+
+    /**
+     * 标签名称
+     */
+    private String tagName;
+}
