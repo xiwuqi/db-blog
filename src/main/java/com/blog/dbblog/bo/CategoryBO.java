fead7c1eaa8efe29d84b3666c6235cf26317ff00
xiwuqi <qiyuewuxi@yeah.net>
2023-08-24 02:02:59 +0800

    新增分类的查询业务类

---
+++ src/main/java/com/blog/dbblog/bo/CategoryBO.java
@@ -0,0 +1,19 @@
+package com.blog.dbblog.bo;
+
+import lombok.Data;
+
+/**
+ * 分类查询条件
+ *
+ * @author wuxi
+ * @create 2023-08-23
+ **/
+
+@Data
+public class CategoryBO {
+
+    /**
+     * 分类名称
+     */
+    private String categoryName;
+}
